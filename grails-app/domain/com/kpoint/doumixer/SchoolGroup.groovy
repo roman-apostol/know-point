3c12dfb47aa961dae6b3d3bce80d82e92dfaf586
Roman Apostol <roman.apostol@gmail.com>
2011-12-18 13:53:48 +0200

    added relation

--- grails-app/domain/com/kpoint/doumixer/SchoolGroup.groovy
+++ grails-app/domain/com/kpoint/doumixer/SchoolGroup.groovy
@@ -7,9 +7,11 @@ class SchoolGroup {
     double price
     String name
     Schedule schedule
+    School school
 
     static constraints = {
         level(inList: ["Elementary", "Pre Intermediate", "Intermediate", "Upper Intermediate", "Advanced"])
         schedule(nullable: true)
+        school(nullable:  true)
     }
 }
