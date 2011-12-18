e7bc1f3ab16239ad8a622acfa318eae6609920c8
Roman Apostol <roman.apostol@gmail.com>
2011-12-18 14:59:13 +0200

    last push

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -59,6 +59,7 @@ grails.exceptionresolver.params.exclude = ['password']
 // enable query caching by default
 grails.hibernate.cache.queries = true
 
+
 // set per-environment serverURL stem for creating absolute links
 environments {
     development {
--- grails-app/domain/com/kpoint/doumixer/Schedule.groovy
+++ grails-app/domain/com/kpoint/doumixer/Schedule.groovy
@@ -11,4 +11,7 @@ class Schedule {
         startDate (blank:false)
         endDate (blank: false)
     }
+    
+    static mapping = {
+    }
 }
